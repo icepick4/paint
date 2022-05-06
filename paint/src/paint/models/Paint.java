b29078ab808d61b6c53ed652f5b17adc5ce408b3
icepick4 <remi.jara@laposte.net>
2022-05-06 12:42:18 +0200

    first commit with structure

---
+++ paint/src/paint/models/Paint.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paint.models;
+import java.awt.Color;
+/**
+ *
+ * @author remi
+ */
+public class Paint {
+    private Color color;
+    private int x;
+    private int y;
+    private int width;
+    private boolean smooth;
+    
+    public Paint(){
+        
+    }
+    
+}
